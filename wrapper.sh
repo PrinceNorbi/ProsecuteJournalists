cf15aa36b4dfb7077acee69d248b767e26c4aaae
PrinceNorbi <norbeey@gmail.com>
2021-03-23 17:20:55 +0100

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,3 @@
 export FLASK_APP=/home/ubuntu/flaskVirtualEnv1/venv/git/ProsecuteJournalists/helloworld.py
 export FLASK_ENV=development
-flask run --host=0.0.0.0
+flask run --host=0.0.0.0 --cert=/etc/letsencrypt/live/ad-astra.hu/fullchain.pem --key=/etc/letsencrypt/live/ad-astra.hu/privkey.pem
