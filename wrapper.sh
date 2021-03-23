8e4ecea424e7d6f30b3e081a995d503985b260f0
PrinceNorbi <norbeey@gmail.com>
2021-03-23 16:27:52 +0100

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,3 @@
-export FLASK_APP=/mnt/d/git/ProsecuteJournalists/helloworld.py
+export FLASK_APP=~/flaskVirtualEnv1/venv/git/ProsecuteJournalists/helloworld.py
 export FLASK_ENV=development
 flask run --host=0.0.0.0 --port=80
